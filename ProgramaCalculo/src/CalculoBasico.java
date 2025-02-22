22c5658dca8225512b3506dba8f50fa8465d0236
Caio Felix <199407954+Caio-Felix1@users.noreply.github.com>
2025-02-22 17:34:16 -0300

    Ajustes na exibição dos Resultados

--- ProgramaCalculo/src/CalculoBasico.java
+++ ProgramaCalculo/src/CalculoBasico.java
@@ -4,34 +4,45 @@ public class CalculoBasico {
 
     public static void soma(Scanner teclado) {
         int num1, num2, soma;
+        String negativo = "";
 
         System.out.print("Digite o primeiro valor (valor inteiro): ");
         num1 = teclado.nextInt();
         System.out.print("Digite o segundo valor (valor inteiro): ");
         num2 = teclado.nextInt();
+
+        System.out.println();
         teclado.nextLine();
 
         soma = num1 + num2;
 
-        System.out.println(num1 + " + " + num2 + " = " + soma);
+        negativo += (num2 < 0)? "(" + num2 + ")": num2;
+
+        System.out.println("Soma: " + num1 + " + " + negativo + " = " + soma);
     }
 
     public static void subtracao(Scanner teclado) {
         int num1, num2, subtracao;
+        String negativo = "";
 
         System.out.print("Digite o primeiro valor (valor inteiro): ");
         num1 = teclado.nextInt();
         System.out.print("Digite o segundo valor (valor inteiro): ");
         num2 = teclado.nextInt();
+
+        System.out.println();
         teclado.nextLine();
 
         subtracao = num1 - num2;
 
-        System.out.println(num1 + " - " + num2 + " = " + subtracao);
+        negativo += (num2 < 0)? "(" + num2 + ")": num2;
+
+        System.out.println("Subtração: " + num1 + " - " + negativo + " = " + subtracao);
     }
 
     public static void divisao(Scanner teclado) {
         float numerador, denominador, divisao;
+        String negativo = "", divisao_formatada;
 
         System.out.print("Digite o numerador (valor real): ");
         numerador = teclado.nextFloat();
@@ -42,24 +53,34 @@ public class CalculoBasico {
                 System.out.println("Não é possível fazer divisão por zero. Digite outro valor!");
             }
         } while (denominador == 0);
+
+        System.out.println();
         teclado.nextLine();
 
         divisao = numerador / denominador;
+        divisao_formatada = String.format("%.2f", divisao);
 
-        System.out.println(numerador + " / " + denominador + " = " + divisao);
+        negativo += (denominador < 0)? "(" + denominador + ")": denominador;
+
+        System.out.println("Divisão: " + numerador + " / " + negativo + " = " + divisao_formatada);
     }
 
     public static void multiplicacao(Scanner teclado) {
         int num1, num2, multiplicacao;
+        String negativo = "";
 
         System.out.print("Digite o primeiro valor (valor inteiro): ");
         num1 = teclado.nextInt();
         System.out.print("Digite o segundo valor (valor inteiro): ");
         num2 = teclado.nextInt();
+
+        System.out.println();
         teclado.nextLine();
 
         multiplicacao = num1 * num2;
 
-        System.out.println(num1 + " X " + num2 + " = " + multiplicacao);
+        negativo += (num2 < 0)? "(" + num2 + ")": num2;
+
+        System.out.println("Multiplicação: " + num1 + " x " + negativo + " = " + multiplicacao);
     }
 }
