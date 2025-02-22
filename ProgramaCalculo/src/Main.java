eedd4c554dd38943d6903b44c00a193f5b42b8dc
Caio Felix <199407954+Caio-Felix1@users.noreply.github.com>
2025-02-22 18:30:47 -0300

    Criação de Funções de Título e de Pausa.

--- ProgramaCalculo/src/Main.java
+++ ProgramaCalculo/src/Main.java
@@ -2,16 +2,31 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void linhas() {
-        System.out.println("----------------------------------------");
+    public static void titulo(String titulo) {
+        int espaco_antes;
+        String linha = "----------------------------------------";
+
+        espaco_antes = (linha.length() - titulo.length()) / 2;
+
+        System.out.println(linha);
+        System.out.println(" ".repeat(espaco_antes) + titulo);
+        System.out.println(linha);
+    }
+
+    public static void pausa(int milisegundos) {
+        try {
+            Thread.sleep(milisegundos);
+        } catch (InterruptedException e) {
+            ;
+        }
     }
 
     public static void tela() {
         Scanner teclado = new Scanner(System.in);
         String num;
+
         do {
-            System.out.println("Cálculos em Java");
-            linhas();
+            titulo("Cálculos em Java");
 
             System.out.println("1 - Soma");
             System.out.println("2 - Subtração");
@@ -26,62 +41,48 @@ public class Main {
 
             switch (num) {
                 case "1":
+                    titulo("Soma");
                     CalculoBasico.soma(teclado);
-                    linhas();
+                    pausa(2500);
                     break;
                 case "2":
+                    titulo("Subtração");
                     CalculoBasico.subtracao(teclado);
-                    linhas();
+                    pausa(2500);
                     break;
                 case "3":
+                    titulo("Divisão");
                     CalculoBasico.divisao(teclado);
-                    linhas();
+                    pausa(2500);
                     break;
                 case "4":
+                    titulo("Multiplicação");
                     CalculoBasico.multiplicacao(teclado);
-                    linhas();
+                    pausa(2500);
                     break;
                 case "5":
+                    titulo("Média Aritmética");
                     CalculoComplexo.media(teclado);
-                    linhas();
-                    try {
-                        Thread.sleep(2500);
-                    } catch (InterruptedException e) {
-                        ;
-                    }
+                    pausa(2500);
                     break;
                 case "6":
+                    titulo("Fatorial");
                     CalculoComplexo.fatorial(teclado);
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        ;
-                    }
-                    linhas();
+                    pausa(2500);
                     break;
                 case "7":
+                    titulo("Bháskara");
                     CalculoComplexo.bhaskara(teclado);
-
-                    try {
-                        Thread.sleep(10000);
-                    } catch (InterruptedException e) {
-                        ;
-                    }
-
-                    linhas();
+                    pausa(10000);
                     break;
                 case "8":
                     System.out.println("Encerrando o programa...");
+                    pausa(1500);
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente!");
-                    linhas();
+                    pausa(1500);
                     break;
-            }
-            try {
-                Thread.sleep(1500);
-            } catch (InterruptedException e) {
-                ;
             }
         } while (!num.equals("8"));
         teclado.close();
